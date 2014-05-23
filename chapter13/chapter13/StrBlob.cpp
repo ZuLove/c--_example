339edc5fb7dc3a798aa39303e359eb5918cecf59
ZuLove <249056903@qq.com>
2014-05-23 18:00:13 +0800

    add StrBlob class

---
+++ chapter13/chapter13/StrBlob.cpp
@@ -0,0 +1,35 @@
+#include "StrBlob.h"
+using namespace std;
+
+StrBlob::StrBlob() : data(make_shared<vector<string>>()) { }
+
+StrBlob::StrBlob(initializer_list<string> il) : data(make_shared<vector<string>>(il)) { }
+
+void StrBlob::check(size_type i, const string &msg) const
+{
+	if (i >= data->size())
+		throw out_of_range(msg);
+}
+
+string& StrBlob::front()
+{
+	check(0, "front on empty StrBlob");
+	return data->front();
+}
+
+string& StrBlob::back()
+{
+	check(0, "back on empty StrBlob");
+	return data->back();
+}
+
+void StrBlob::pop_back()
+{
+	check(0, "pop_back on empty StrBlob");
+	return data->pop_back();
+}
+int main()
+{
+
+	return 0;
+}
